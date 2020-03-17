28c766af2c422ab130fde8d9aecfd2372e9ccb3f
Leonardo Santos <sds.leo2@gmail.com>
2020-03-17 13:35:20 -0300

    ajustes

--- android/build/generated/source/buildConfig/debug/com/reactlibrary/BuildConfig.java
+++ android/build/generated/source/buildConfig/debug/com/reactlibrary/BuildConfig.java
@@ -1,11 +1,11 @@
 /**
  * Automatically generated file. DO NOT MODIFY
  */
-package com.reactlibrary;
+package br.com.loreweb.backgroundprocess;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.reactlibrary";
+  public static final String APPLICATION_ID = "br.com.loreweb.backgroundprocess";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
